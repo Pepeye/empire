dd960668bb636ec6396e64502da950d8bff2babd
Pepeye <gaofolaju@gmail.com>
2019-02-28 21:08:52 +0000

    feat: added server ping at root path
    GET / responds with a server ping {timestamp, message, id}

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use actix_web::{server, App, HttpRequest, HttpResponse, Responder};
+use uuid::Uuid;
+use chrono::prelude::*;
+use serde_derive::{Serialize, Deserialize};
+
+#[derive(Serialize, Deserialize)]
+struct Ping {
+    id: String,
+    message: String,
+    time: String,
+}
+
+fn ping(_req: &HttpRequest) -> impl Responder {
+    // let to = req.match_info().get("name").unwrap_or("World");
+    // format!("Hello {}!", to)
+    let p: Ping = Ping {
+        id: Uuid::new_v4().to_string(),
+        message: "empire.service".to_owned(),
+        time: Utc::now().to_string()
+    };
+    HttpResponse::Ok()
+        .content_type("application/json")
+        .json(p)
+}
+
 fn main() {
-    println!("Hello, world!");
+    server::new(|| {
+        App::new()
+            .resource("/", |r| r.f(ping))
+            .resource("/{name}", |r| r.f(ping))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Can not bind to port 8000")
+    .run();
 }
