3c4957433fe84dcfff4a384dbc36fa2e6c5b058f
Pepeye <gaofolaju@gmail.com>
2019-03-01 19:10:24 +0000

    feat: adds logger middleware / cargo-watch
    - adds logging middleware to server
    - adds cargo-watch to the project

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,19 @@
 use actix_web::{server, App, HttpRequest, HttpResponse, Responder};
+use actix_web::middleware::Logger;
+use env_logger;
 use uuid::Uuid;
 use chrono::prelude::*;
 use serde_derive::{Serialize, Deserialize};
 
 #[derive(Serialize, Deserialize)]
-struct Ping {
+struct Pong {
     id: String,
     message: String,
-    time: String,
+    time: String
 }
 
 fn pong(_req: &HttpRequest) -> impl Responder {
-    let p: Ping = Ping {
+    let p: Pong = Pong {
         id: Uuid::new_v4().to_string(),
         message: "empire.service".to_owned(),
         time: Utc::now().to_string()
@@ -22,8 +24,12 @@ fn pong(_req: &HttpRequest) -> impl Responder {
 }
 
 fn main() {
+    std::env::set_var("RUST_LOG", "actix_web=info");
+    env_logger::init();
+    
     server::new(|| {
         App::new()
+            .middleware(Logger::new("%a %r %{User-Agent}i %Dms"))
             .resource("/", |r| r.f(pong))
             .resource("/ping", |r| r.f(pong))
     })
